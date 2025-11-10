28ed0831d6de3c4fa34fa48c6892067da9b3120d
fjrhub <muhammadfajrulfalah@proton.me>
2025-11-10 14:36:51 +0700

    chore(api-toggle): disable siputzx facebook testing

--- commands/auto.js
+++ commands/auto.js
@@ -416,7 +416,7 @@ module.exports = {
       const enableStatus = {
         tiktok: { siputzx: true, archive: true, vreden: true },
         instagram: { siputzx: false, archive: true, vreden: false },
-        facebook: { siputzx: true, archive: true, vreden: true },
+        facebook: { siputzx: false, archive: true, vreden: true },
       };
 
       const apis = [];
